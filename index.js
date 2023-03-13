17a16bd8dea9b89487abc7ef72a75704ce482430
Vidhi Sharma <vidhisharma@Vidhis-MacBook-Air.local>
2023-03-14 00:50:00 +0530

    a

---
+++ index.js
@@ -0,0 +1,17 @@
+/* --------------------------------------------------RESUME-DOWNLOAD------------------------------------------------------ */
+
+document.getElementById("resume-link-1").addEventListener("click", () => {
+  window.location.assign(
+    "https://drive.google.com/file/d/11LSC_CpTntfSHYcNwM70oB2-jbGvYTsu/view?usp=share_link",
+    "_blank"
+  );
+});
+
+document.getElementById("resume-link-2").addEventListener("click", () => {
+  window.location.assign(
+    "https://drive.google.com/file/d/11LSC_CpTntfSHYcNwM70oB2-jbGvYTsu/view?usp=share_link",
+    "_blank"
+  );
+});
+
+/* --------------------------------------------------RESUME-DOWNLOAD------------------------------------------------------ */
